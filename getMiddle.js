a3f0e63743a61e0f17bd2ffbe0c6bea465379f8b
ShlomiR <13311112+shlomi-r@users.noreply.github.com>
2023-04-18 06:23:55 +0300

    Create getMiddle.js

---
+++ getMiddle.js
@@ -0,0 +1,16 @@
+/*
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+*/
+function getMiddle(s) {
+  return s.length % 2 == 0
+    ? s.substr(parseInt(s.length / 2) - 1, 2)
+    : s.substr(parseInt(s.length / 2), 1);
+}
+// getMiddle("test"); // => "es"
+// getMiddle("taste"); // => "s"
