1e308a4c2384381cb3778beaf55332198d544815
ShlomiR <13311112+shlomi-r@users.noreply.github.com>
2023-06-18 12:17:10 +0300

    Update highestWord.js

--- highestWord.js
+++ highestWord.js
@@ -2,14 +2,14 @@ function high(x){
   //           00000000011111111112222222222
   //           12345678901234567890123456789
   const map = 'abcdefghijklmnopqrstuvwxyz';
-  const wordWeights = x.split(' ').reduce((obj, key) => {  
-      obj.push(
+  const wordWeights = x.split(' ').reduce((arr, key) => {  
+      arr.push(
       { 
         word: key, 
         value: key.split('').map(x => map.indexOf(x)+1).reduce((a,c) => a+c, 0 ) 
       }
       ) ;
-      return obj;
+      return arr;
   }, []);
   
   wordWeights.sort((a,b) => b.value - a.value); //highest weight word goes first (DESC)
